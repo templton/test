be8394a7b69b0c379e75293325385de28e31f30c
Dmitry Salamatov <dimasalam@yandex.ru>
2024-05-15 21:57:29 +0700

    EOL for windows

--- src/TestClass.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App;
-
-class TestClass
-{
-    public int $start = 10;
-}
--- src/index.php
+++ src/index.php
@@ -1,14 +1,5 @@
 <?php
 
-use App\Controllers\CarController;
-use App\Enum\BodyTypeEnum;
-
 require __DIR__ . '/vendor/autoload.php';
 
-$_GET['bodyType'] = BodyTypeEnum::HATCHBACK;
-$_GET['capacity'] = 99;
-$_GET['doorsCount'] = 4;
-
-$controlCar = new CarController();
-
-$controlCar->create();
+echo 'works!';
